2d5cd60cea7991bc81417e7c691137bc9c93ad94
allanle <ale11@my.bcit.ca>
2016-01-08 00:29:05 -0800

    Removed index.phh and added into footer.php and header.php

---
+++ template/footer.php
@@ -0,0 +1,12 @@
+</div>
+        <div class="cl">&#160;</div>
+    </div>
+</div>
+    
+<div id="footer">
+    <div id="footer_in">
+        &copy; <a href="http://www.google.ca" title="eCommerce" target="_blank">Allan Le</a>
+    </div>
+</div>
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml`/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1991/xhtml">
+<head>
+    <meta http-equib="Content-Type" content="text/html; charset=UTF-8" />
+    <title>eCommerce</title>
+    <meta name="description" content="eCommerce website project" />
+    <meta name="keywords" content="eCommerce website project" />
+    <meta http-equiv="imagetoolbar" content="no" />
+    <link href="/css/core.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div id="header">
+        <div id="header_in">
+            <h5><a href="/">Business name</a></h5>
+        </div>
+    </div>
+    
+    <div id="outer">
+        <div id="wrapper">
+            <div id="left">
+                <h2>Categories</h2>
+                <ul id="navigation">
+                    <li><a href="#">link</a></li>
+                </ul>
+            </div>
+            
+            <div id="right">
