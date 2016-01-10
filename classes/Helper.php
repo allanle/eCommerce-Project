951ee504a407590ca5625b353ce55fda59bdc101
allanle <ale11@my.bcit.ca>
2016-01-09 23:05:37 -0800

    Added Helper.php class

---
+++ classes/Helper.php
@@ -0,0 +1,41 @@
+<?php
+class Helper {
+    
+    public static function getActive($page = null) {
+        if(!empty($page)) {
+            if(is_array($page)) {
+                $error = array();
+                foreach($page as $key =? $value) {
+                    if(Url::getParam($key) != $value) {
+                        array_push($error, $key);
+                    }
+                }
+                return empty($error) ? " class=\"act\"" : null;
+            }
+        }
+        return $page == Url::cPage() ? " class=\"act\"" : null;
+    }
+	
+	public static function encodeHTML($string, $case = 2) {
+		switch($case) {
+			case 1:
+			return htmlentities($string, ENT_NOQUOTES, 'UTF-8', false);
+			break;			
+			case 2:
+			$pattern = '<([a-zA-Z0-9\.\, "\'_\/\-\+~=;:\(\)?&#%![\]@]+)>';
+			// put text only, devided with html tags into array
+			$textMatches = preg_split('/' . $pattern . '/', $string);
+			// array for sanitised output
+			$textSanitised = array();			
+			foreach($textMatches as $key => $value) {
+				$textSanitised[$key] = htmlentities(html_entity_decode($value, ENT_QUOTES, 'UTF-8'), ENT_QUOTES, 'UTF-8');
+			}			
+			foreach($textMatches as $key => $value) {
+				$string = str_replace($value, $textSanitised[$key], $string);
+			}			
+			return $string;			
+			break;
+		}
+	}
+	
+}
