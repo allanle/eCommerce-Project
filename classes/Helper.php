d7f730197c2403e78553f3e450e19c9329817db0
allanle <ale11@my.bcit.ca>
2016-01-11 12:30:39 -0800

    29-33

--- classes/Helper.php
+++ classes/Helper.php
@@ -46,4 +46,13 @@ class Helper {
 		}
 	}
 	
+    public static function shortenString($string, $len = 150) {
+        if(strlen($string) > $len) {
+            $string = trim(substring($string, 0, $len));
+            $string = substr($string, 0, strrpos($string, " "))."&hellip;";
+        } else {
+            $string .= "&hellip;";
+        }
+        return $string;
+    }
 }
--- template/_footer.php
+++ template/_footer.php
@@ -4,7 +4,7 @@
 </div>
 <div id="footer">
 	<div id="footer_in">
-		&copy; <a href="http://www.sebastiansulinski.co.uk" title="Web Design Tutorials" target="_blank">Sebastian Sulinski</a>
+		&copy; <a href="http://www.google.ca" title="Web Design Tutorials" target="_blank">Hi</a>
 	</div>
 </div>
 </body>
